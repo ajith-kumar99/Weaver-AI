ededaaf2d4a7d25240ef514de2a2635a6a52751a
ajith-kumar99 <ajithkumarguntipalli@gmail.com>
2025-10-18 18:56:45 +0530

    Backend Integrated with frontend

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
 import Message from './Message'
+import toast from 'react-hot-toast'
 
 
 
@@ -9,7 +10,7 @@ import Message from './Message'
 const ChatBox = () => {
 
   const containerRef = useRef(null)
-  const { selectedChat, theme } = useAppContext()
+  const { selectedChat, theme,axios,token,setUser,user } = useAppContext()
 
   const [messages, setMessages] = useState([])
   const [loading, setLoading] = useState(false)
@@ -19,7 +20,35 @@ const ChatBox = () => {
   const [isPublished,setIsPublished] = useState(false)
 
   const onSubmit = async (e) =>{
-    e.preventDefault()
+    try {
+      e.preventDefault()
+      if(!user) return toast('Login to send Message')
+      setLoading(true)
+      const promptCopy = prompt
+      setPrompt('')
+      setMessages(prev => [...prev,{role:'user',content:prompt,timestamp:Date.now(),isImage:false}])  
+
+      const {data} = await axios.post(`/api/message/${mode}`, {chatId:selectedChat._id,prompt,isPublished},{headers:{Authorization:token}})
+
+      if(data.success){
+        setMessages(prev => [...prev,data.reply])
+        // decrease credits
+        if(mode === 'image'){
+          setUser(prev => ({...prev,credits: prev.credits - 2}))
+        }else{
+          setUser(prev => ({...prev,credits: prev.credits - 1}))
+        }
+      }else{
+        toast.error(data.message)
+        setPrompt(promptCopy)
+      }
+    } catch (error) {
+       toast.error(error.message)
+    }finally{
+      setPrompt('')
+      setLoading(false)
+    }
+  
   }
 
   useEffect(() => {
